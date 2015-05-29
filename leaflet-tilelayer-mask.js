50c3e9c03bf8547b140048cfaf2349124b5952b8
frogcat <frogcat@users.noreply.github.com>
2015-05-29 11:09:39 +0900

    add options

--- leaflet-tilelayer-mask.js
+++ leaflet-tilelayer-mask.js
@@ -1,95 +1,113 @@
 (function(window) {
 
-	var NS_SVG = "http://www.w3.org/2000/svg";
-	var NS_XLINK = "http://www.w3.org/1999/xlink";
+	// Utility
 
+	var maskUrlDefault = 'data:image/png;base64,'
+			+ 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAC7lBMVEUAAAABAQECAgIDAwMEBAQF'
+			+ 'BQUGBgYHBwcICAgJCQkKCgoLCwsMDAwNDQ0ODg4PDw8QEBARERESEhITExMUFBQVFRUWFhYXFxcY'
+			+ 'GBgZGRkaGhobGxscHBwdHR0eHh4fHx8gICAhISEiIiIjIyMkJCQlJSUmJiYnJycoKCgpKSkqKior'
+			+ 'KyssLCwtLS0uLi4vLy8wMDAxMTEyMjIzMzM0NDQ1NTU2NjY3Nzc4ODg5OTk6Ojo7Ozs8PDw9PT0+'
+			+ 'Pj4/Pz9AQEBBQUFCQkJDQ0NERERGRkZHR0dISEhJSUlKSkpLS0tMTExNTU1OTk5PT09QUFBRUVFS'
+			+ 'UlJTU1NUVFRVVVVWVlZXV1dYWFhZWVlaWlpbW1tcXFxdXV1eXl5fX19gYGBhYWFiYmJjY2NkZGRl'
+			+ 'ZWVmZmZnZ2doaGhpaWlqampra2tsbGxtbW1ubm5vb29xcXFycnJzc3N0dHR1dXV2dnZ3d3d4eHh5'
+			+ 'eXl6enp7e3t8fHx9fX1+fn5/f3+AgICBgYGCgoKDg4OEhISFhYWGhoaHh4eIiIiKioqLi4uMjIyN'
+			+ 'jY2Ojo6Pj4+QkJCRkZGSkpKTk5OUlJSVlZWWlpaXl5eYmJiZmZmampqbm5ucnJydnZ2enp6fn5+g'
+			+ 'oKChoaGioqKkpKSlpaWmpqanp6eoqKipqamqqqqrq6usrKytra2urq6vr6+wsLCxsbGysrKzs7O0'
+			+ 'tLS1tbW2tra3t7e4uLi5ubm6urq7u7u9vb2+vr6/v7/AwMDBwcHCwsLDw8PExMTFxcXGxsbHx8fI'
+			+ 'yMjJycnKysrLy8vMzMzNzc3Pz8/Q0NDR0dHS0tLT09PU1NTV1dXW1tbX19fY2NjZ2dna2trb29vc'
+			+ '3Nzd3d3e3t7f39/g4ODh4eHi4uLj4+Pk5OTl5eXm5ubn5+fo6Ojp6enq6urr6+vs7Ozt7e3u7u7v'
+			+ '7+/w8PDx8fHy8vLz8/P09PT19fX29vb39/f4+Pj5+fn6+vr7+/v8/Pz9/f3+/v7///9A5nLSAAAA'
+			+ 'AWJLR0QAiAUdSAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB98FHBEuKjDLarAAAAAdaVRY'
+			+ 'dENvbW1lbnQAAAAAAENyZWF0ZWQgd2l0aCBHSU1QZC5lBwAAAjNJREFUOMtjYEACjExMLCzMTIwM'
+			+ '2AETK7eQhIyslAgvOzM2aU4xTbvAhMyspFAXPWkedCWM7GJGYVUz1+0+fGTfpvnNCdYynEwo8twq'
+			+ 'fo1rTt978fbDh3evHl7c1hOty49kCCOPTtK8U08+fPvx4+fPnz++fXpxaXWhhRAzQr9O5uob7779'
+			+ 'hIPvHx/sqLISgNnCrpK8+s6nHz+RwI8vT3aVGnJDPMwk5jfvBqo8SMWjDUkKrGAFnEYNJ9+hyQNV'
+			+ 'fLo+zUUAZASTeNjaJ99+YoDvr/flKLEAFbBpVp368BML+HJzohU3UAG33Yy7X7Ep+P58XZAwEwOT'
+			+ 'UOC65z+wKfj5/lCmLDMDk0T87rfYFXw6W6HGysAik3kYqxOAjrjcpM3GwCKbdQSXgivNOuwMTFLJ'
+			+ '+95ht+Lz+Wp1VgYmkdBNL7Er+HA0V56FgZHXef7Db9jkf7zaEikGjC92vaYLn7Ap+HpvliMPMKyZ'
+			+ 'peO3vfiOxYB3x8s02ECRxWPdc+kjpiu+3FvoKwJOESwy0asefEGX//Zid74GOyTBcOoW7njyFdWM'
+			+ 'b6+Ot9rwQ3MIE79F9a5Hn74jJ5cXx3vcxeCJklnIqnTD9TdfoEp+fH13b3ebuxQrIlkzCxgmTdt3'
+			+ '4/n7T1++fP7w6u6xhfk24qwo+YpbwSVn4rqDZy9fOX90y6wyXw1+tLzFyMqvZBWcUdHUXJ0b6agh'
+			+ 'zI6ZgxlZuIRlVbW11eXFeFhxZHAmZlY2dlYWlPwPAD6nKPWk11d/AAAAAElFTkSuQmCC';
+
+	var count = new Date().getTime();
 	var $e = function(name, attr) {
-		var e = document.createElementNS(NS_SVG, name);
+		var id = "_leaflet_tilelayer_mask_" + (count++);
+		if (document.getElementById(id) != null)
+			return $e(name, attr);
+		var e = document.createElementNS("http://www.w3.org/2000/svg", name);
+		e.setAttribute("id", id);
 		if (attr)
 			for (key in attr)
 				if (key == "href")
-					e.setAttributeNS(NS_XLINK, key, attr[key]);
+					e.setAttributeNS("http://www.w3.org/1999/xlink", key, attr[key]);
 				else
 					e.setAttribute(key, attr[key]);
 		return e;
 	};
 
+	// Main
+
 	L.TileLayer.Mask = L.TileLayer.extend({
-		initialize : function(url, options, mask, width, height) {
-			L.TileLayer.prototype.initialize.call(this, url, options);
-			this._mask = mask;
-			this._width = width;
-			this._height = height;
+		options : {
+			maskWidth : 512,
+			maskHeight : 512,
+			maskUrl : maskUrlDefault
 		},
-		_onTileLoad : function(e) {
-			if (this._e5) {
-				var p = L.DomUtil.getPosition(e.tile);
-				e.tile.img = $e("image", {
-					x : p.x,
-					y : p.y,
-					width : 256,
-					height : 256,
-					href : e.url
-				});
-				this._e5.appendChild(e.tile.img);
+		_tileEventHandler : {
+			"tileload" : function(e) {
+				if (this._e5) {
+					var p = L.DomUtil.getPosition(e.tile);
+					e.tile._svgimg = $e("image", {
+						x : p.x,
+						y : p.y,
+						width : this.options.tileSize,
+						height : this.options.tileSize,
+						href : e.url
+					});
+					this._e5.appendChild(e.tile._svgimg);
+				}
+			},
+			"tileunload" : function(e) {
+				var img = e.tile._svgimg;
+				if (img && img.parentNode)
+					img.parentNode.removeChild(img);
 			}
 		},
-		_onTileUnload : function(e) {
-			var img = e.tile.img;
-			if (img && img.parentNode)
-				img.parentNode.removeChild(img);
-		},
-		_onMove : function(e) {
-			if (this._e7 && this._map) {
-				var p = this._map.containerPointToLayerPoint([ 0, 0 ]);
-				this._e7.setAttribute("x", -p.x);
-				this._e7.setAttribute("y", -p.y);
+		_mapEventHandler : {
+			"move" : function(e) {
+				if (this._e7 && this._map) {
+					var p = this._map.containerPointToLayerPoint([ 0, 0 ]);
+					this._e7.setAttribute("x", -p.x);
+					this._e7.setAttribute("y", -p.y);
+				}
+			},
+			"viewreset" : function(e) {
+				if (this._e5)
+					while (this._e5.firstChild)
+						this._e5.removeChild(this._e5.firstChild);
+			},
+			"zoomstart" : function(e) {
+				if (this._e1)
+					this._e1.style.opacity = 0.5;
+			},
+			"zoomend" : function(e) {
+				if (this._e1)
+					this._e1.style.opacity = 1.0;
 			}
-		},
-		_onViewReset : function(e) {
-			if (this._e5)
-				while (this._e5.firstChild)
-					this._e5.removeChild(this._e5.firstChild);
-		},
-		_onZoomStart : function(e) {
-			if (this._e1)
-				this._e1.style.opacity = 0.5;
-		},
-		_onZoomEnd : function(e) {
-			if (this._e1)
-				this._e1.style.opacity = 1.0;
 		},
 		onRemove : function(map) {
 			L.TileLayer.prototype.onRemove.call(this, map);
 			if (this._e1 && this._e1.parentNode)
 				this._e1.parentNode.removeChild(this._e1);
-			this.off({
-				"tileload" : this._onTileLoad,
-				"tileunload" : this._onTileUnload
-			}, this);
-			map.off({
-				"move" : this._onMove,
-				"viewreset" : this._onViewReset,
-				"zoomstart" : this._onZoomStart,
-				"zoomend" : this._onZoomEnd
-			}, this);
+			this.off(this._tileEventHandler, this);
+			map.off(this._mapEventHandler, this);
 		},
 		onAdd : function(map) {
-
 			L.TileLayer.prototype.onAdd.call(this, map);
 			this.getContainer().style.display = "none";
-			this.on({
-				"tileload" : this._onTileLoad,
-				"tileunload" : this._onTileUnload
-			}, this);
-			map.on({
-				"move" : this._onMove,
-				"viewreset" : this._onViewReset,
-				"zoomstart" : this._onZoomStart,
-				"zoomend" : this._onZoomEnd
-			}, this);
-
-			this._map = map;
+			this.on(this._tileEventHandler, this);
+			map.on(this._mapEventHandler, this);
 
 			this._e1 = $e("svg", {
 				width : "100%",
@@ -98,27 +116,20 @@
 			});
 			this._e2 = $e("defs");
 			this._e3 = $e("mask", {
-				"id" : "e3",
-				"maskUnits" : "userSpaceOnUse"
+				maskUnits : "userSpaceOnUse"
 			});
 			this._e4 = $e("image", {
-				x : -0.5 * this._width,
-				y : -0.5 * this._height,
-				width : this._width,
-				height : this._height,
-				href : this._mask,
+				width : this.options.maskWidth,
+				height : this.options.maskHeight,
+				href : this.options.maskUrl,
 				preserveAspectRatio : "none"
 			});
-			this._e5 = $e("g", {
-				"id" : "e5"
-			});
+			this._e5 = $e("g");
 			this._e6 = $e("g", {
-				"mask" : "url(#e3)"
+				mask : "url(#" + this._e3.getAttribute("id") + ")"
 			});
 			this._e7 = $e("use", {
-				"x" : 0,
-				"y" : 0,
-				href : "#e5"
+				href : "#" + this._e5.getAttribute("id")
 			});
 			this._e1.appendChild(this._e2);
 			this._e2.appendChild(this._e3);
@@ -127,12 +138,14 @@
 			this._e1.appendChild(this._e6);
 			this._e6.appendChild(this._e7);
 
-			map.getContainer().appendChild(this._e1);
+			var cnt = map.getContainer();
+			cnt.appendChild(this._e1);
+			this.setCenter(cnt.clientWidth * 0.5, cnt.clientHeight * 0.5);
 		},
 		setCenter : function(x, y) {
 			if (this._e4) {
-				this._e4.setAttribute("x", x - this._width * 0.5);
-				this._e4.setAttribute("y", y - this._height * 0.5);
+				this._e4.setAttribute("x", x - this.options.maskWidth * 0.5);
+				this._e4.setAttribute("y", y - this.options.maskHeight * 0.5);
 			}
 			return this;
 		}

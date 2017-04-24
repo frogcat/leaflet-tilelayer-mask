16257d99e5efcbc8c0d7632b91d93155a9b558ec
frogcat <frogcat@users.noreply.github.com>
2017-04-24 19:19:16 +0900

    Upgrade v0.7 to v1.0

--- README.md
+++ README.md
@@ -3,33 +3,32 @@
 Leaflet-tilelayer-mask adds mask effect to tilelayer.
 
 
-![Leaflet-tilelayer-mask](http://frogcat.github.io/leaflet-tilelayer-mask/screenshot.jpg)
+![Leaflet-tilelayer-mask](http://frogcat.github.io/leaflet-tilelayer-mask/misc/screenshot.jpg)
 
 
 
 ## Demo
 
-http://frogcat.github.io/leaflet-tilelayer-mask/
+<http://frogcat.github.io/leaflet-tilelayer-mask/>
 
 ## Usage
 
     // Create background tile layer
       var bg  L.tileLayer(...);
-      
+
       // Create foreground tile layer with mask
       var fg = L.tileLayer.mask('http://www.finds.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/{z}/{x}/{y}.png', {
         	  maskUrl : 'star.png', // optional
-        	  maskWidth : 800,  //optional
-        	  maskHeight : 800 //optional
+        	  maskSize : 1024  //optional
           });
-          
+
       // Create map with layers and bind listener to update center of mask
       var map = L.map("map", {
      	          zoom : ...,
      	  center : ...,
      	  layers : [ bg, fg ]
        }).on("mousemove", function(e) {
-     	  fg.setCenter(e.containerPoint.x, e.containerPoint.y);
+     	  fg.setCenter(e.containerPoint);
        });
 
 ## Options
@@ -37,22 +36,18 @@ http://frogcat.github.io/leaflet-tilelayer-mask/
 Option  | Type | Default | Description
 --------|------|---------|-------------
 maskUrl  | String | #1 |  Url of mask image
-maskWidth  | Number | 512 | mask width
-maskHeight  | Number | 512 | mask height
+maskSize  | L.point or Number | L.point(512,512) | mask size
 
-Note #1 : Built in image 'data:image/png;base64,...' (white circle with soft edge) 
+Note #1 : Built in image 'data:image/png;base64,...' (white circle with soft edge)
 
 ## Methods
 
-Mthod  | Returns | Description
+Method  | Returns | Description
 ----- | -------------|------
-setCenter(x,y)  | this | Set the mask center relative to map container.
+setCenter(Number x, Number y)  | this | Set the mask center relative to map container.
+setCenter(Point p)  | this | Expects a L.Point object  instead
 
 
-
-
-## Important notice
+## Notice
 
 * Leaflet-tilelayer-mask depends on SVG mask.
-* To improve rendering performance, tile layer container is added as a child of map container.
-* Other layers (include Marker, Popup, Vector) will be shown behind the masked tile layer.
--- leaflet-tilelayer-mask.js
+++ leaflet-tilelayer-mask.js
@@ -1,157 +1,124 @@
-(function(window) {
+(function() {
 
-	// Utility
+  var defaultMaskUrl = [
+    'data:image/png;base64,',
+    'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAC7lBMVEUAAAABAQECAgIDAwMEBAQF',
+    'BQUGBgYHBwcICAgJCQkKCgoLCwsMDAwNDQ0ODg4PDw8QEBARERESEhITExMUFBQVFRUWFhYXFxcY',
+    'GBgZGRkaGhobGxscHBwdHR0eHh4fHx8gICAhISEiIiIjIyMkJCQlJSUmJiYnJycoKCgpKSkqKior',
+    'KyssLCwtLS0uLi4vLy8wMDAxMTEyMjIzMzM0NDQ1NTU2NjY3Nzc4ODg5OTk6Ojo7Ozs8PDw9PT0+',
+    'Pj4/Pz9AQEBBQUFCQkJDQ0NERERGRkZHR0dISEhJSUlKSkpLS0tMTExNTU1OTk5PT09QUFBRUVFS',
+    'UlJTU1NUVFRVVVVWVlZXV1dYWFhZWVlaWlpbW1tcXFxdXV1eXl5fX19gYGBhYWFiYmJjY2NkZGRl',
+    'ZWVmZmZnZ2doaGhpaWlqampra2tsbGxtbW1ubm5vb29xcXFycnJzc3N0dHR1dXV2dnZ3d3d4eHh5',
+    'eXl6enp7e3t8fHx9fX1+fn5/f3+AgICBgYGCgoKDg4OEhISFhYWGhoaHh4eIiIiKioqLi4uMjIyN',
+    'jY2Ojo6Pj4+QkJCRkZGSkpKTk5OUlJSVlZWWlpaXl5eYmJiZmZmampqbm5ucnJydnZ2enp6fn5+g',
+    'oKChoaGioqKkpKSlpaWmpqanp6eoqKipqamqqqqrq6usrKytra2urq6vr6+wsLCxsbGysrKzs7O0',
+    'tLS1tbW2tra3t7e4uLi5ubm6urq7u7u9vb2+vr6/v7/AwMDBwcHCwsLDw8PExMTFxcXGxsbHx8fI',
+    'yMjJycnKysrLy8vMzMzNzc3Pz8/Q0NDR0dHS0tLT09PU1NTV1dXW1tbX19fY2NjZ2dna2trb29vc',
+    '3Nzd3d3e3t7f39/g4ODh4eHi4uLj4+Pk5OTl5eXm5ubn5+fo6Ojp6enq6urr6+vs7Ozt7e3u7u7v',
+    '7+/w8PDx8fHy8vLz8/P09PT19fX29vb39/f4+Pj5+fn6+vr7+/v8/Pz9/f3+/v7///9A5nLSAAAA',
+    'AWJLR0QAiAUdSAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB98FHBEuKjDLarAAAAAdaVRY',
+    'dENvbW1lbnQAAAAAAENyZWF0ZWQgd2l0aCBHSU1QZC5lBwAAAjNJREFUOMtjYEACjExMLCzMTIwM',
+    '2AETK7eQhIyslAgvOzM2aU4xTbvAhMyspFAXPWkedCWM7GJGYVUz1+0+fGTfpvnNCdYynEwo8twq',
+    'fo1rTt978fbDh3evHl7c1hOty49kCCOPTtK8U08+fPvx4+fPnz++fXpxaXWhhRAzQr9O5uob7779',
+    'hIPvHx/sqLISgNnCrpK8+s6nHz+RwI8vT3aVGnJDPMwk5jfvBqo8SMWjDUkKrGAFnEYNJ9+hyQNV',
+    'fLo+zUUAZASTeNjaJ99+YoDvr/flKLEAFbBpVp368BML+HJzohU3UAG33Yy7X7Ep+P58XZAwEwOT',
+    'UOC65z+wKfj5/lCmLDMDk0T87rfYFXw6W6HGysAik3kYqxOAjrjcpM3GwCKbdQSXgivNOuwMTFLJ',
+    '+95ht+Lz+Wp1VgYmkdBNL7Er+HA0V56FgZHXef7Db9jkf7zaEikGjC92vaYLn7Ap+HpvliMPMKyZ',
+    'peO3vfiOxYB3x8s02ECRxWPdc+kjpiu+3FvoKwJOESwy0asefEGX//Zid74GOyTBcOoW7njyFdWM',
+    'b6+Ot9rwQ3MIE79F9a5Hn74jJ5cXx3vcxeCJklnIqnTD9TdfoEp+fH13b3ebuxQrIlkzCxgmTdt3',
+    '4/n7T1++fP7w6u6xhfk24qwo+YpbwSVn4rqDZy9fOX90y6wyXw1+tLzFyMqvZBWcUdHUXJ0b6agh',
+    'zI6ZgxlZuIRlVbW11eXFeFhxZHAmZlY2dlYWlPwPAD6nKPWk11d/AAAAAElFTkSuQmCC'
+  ].join("");
 
-	var maskUrlDefault = 'data:image/png;base64,'
-			+ 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAC7lBMVEUAAAABAQECAgIDAwMEBAQF'
-			+ 'BQUGBgYHBwcICAgJCQkKCgoLCwsMDAwNDQ0ODg4PDw8QEBARERESEhITExMUFBQVFRUWFhYXFxcY'
-			+ 'GBgZGRkaGhobGxscHBwdHR0eHh4fHx8gICAhISEiIiIjIyMkJCQlJSUmJiYnJycoKCgpKSkqKior'
-			+ 'KyssLCwtLS0uLi4vLy8wMDAxMTEyMjIzMzM0NDQ1NTU2NjY3Nzc4ODg5OTk6Ojo7Ozs8PDw9PT0+'
-			+ 'Pj4/Pz9AQEBBQUFCQkJDQ0NERERGRkZHR0dISEhJSUlKSkpLS0tMTExNTU1OTk5PT09QUFBRUVFS'
-			+ 'UlJTU1NUVFRVVVVWVlZXV1dYWFhZWVlaWlpbW1tcXFxdXV1eXl5fX19gYGBhYWFiYmJjY2NkZGRl'
-			+ 'ZWVmZmZnZ2doaGhpaWlqampra2tsbGxtbW1ubm5vb29xcXFycnJzc3N0dHR1dXV2dnZ3d3d4eHh5'
-			+ 'eXl6enp7e3t8fHx9fX1+fn5/f3+AgICBgYGCgoKDg4OEhISFhYWGhoaHh4eIiIiKioqLi4uMjIyN'
-			+ 'jY2Ojo6Pj4+QkJCRkZGSkpKTk5OUlJSVlZWWlpaXl5eYmJiZmZmampqbm5ucnJydnZ2enp6fn5+g'
-			+ 'oKChoaGioqKkpKSlpaWmpqanp6eoqKipqamqqqqrq6usrKytra2urq6vr6+wsLCxsbGysrKzs7O0'
-			+ 'tLS1tbW2tra3t7e4uLi5ubm6urq7u7u9vb2+vr6/v7/AwMDBwcHCwsLDw8PExMTFxcXGxsbHx8fI'
-			+ 'yMjJycnKysrLy8vMzMzNzc3Pz8/Q0NDR0dHS0tLT09PU1NTV1dXW1tbX19fY2NjZ2dna2trb29vc'
-			+ '3Nzd3d3e3t7f39/g4ODh4eHi4uLj4+Pk5OTl5eXm5ubn5+fo6Ojp6enq6urr6+vs7Ozt7e3u7u7v'
-			+ '7+/w8PDx8fHy8vLz8/P09PT19fX29vb39/f4+Pj5+fn6+vr7+/v8/Pz9/f3+/v7///9A5nLSAAAA'
-			+ 'AWJLR0QAiAUdSAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB98FHBEuKjDLarAAAAAdaVRY'
-			+ 'dENvbW1lbnQAAAAAAENyZWF0ZWQgd2l0aCBHSU1QZC5lBwAAAjNJREFUOMtjYEACjExMLCzMTIwM'
-			+ '2AETK7eQhIyslAgvOzM2aU4xTbvAhMyspFAXPWkedCWM7GJGYVUz1+0+fGTfpvnNCdYynEwo8twq'
-			+ 'fo1rTt978fbDh3evHl7c1hOty49kCCOPTtK8U08+fPvx4+fPnz++fXpxaXWhhRAzQr9O5uob7779'
-			+ 'hIPvHx/sqLISgNnCrpK8+s6nHz+RwI8vT3aVGnJDPMwk5jfvBqo8SMWjDUkKrGAFnEYNJ9+hyQNV'
-			+ 'fLo+zUUAZASTeNjaJ99+YoDvr/flKLEAFbBpVp368BML+HJzohU3UAG33Yy7X7Ep+P58XZAwEwOT'
-			+ 'UOC65z+wKfj5/lCmLDMDk0T87rfYFXw6W6HGysAik3kYqxOAjrjcpM3GwCKbdQSXgivNOuwMTFLJ'
-			+ '+95ht+Lz+Wp1VgYmkdBNL7Er+HA0V56FgZHXef7Db9jkf7zaEikGjC92vaYLn7Ap+HpvliMPMKyZ'
-			+ 'peO3vfiOxYB3x8s02ECRxWPdc+kjpiu+3FvoKwJOESwy0asefEGX//Zid74GOyTBcOoW7njyFdWM'
-			+ 'b6+Ot9rwQ3MIE79F9a5Hn74jJ5cXx3vcxeCJklnIqnTD9TdfoEp+fH13b3ebuxQrIlkzCxgmTdt3'
-			+ '4/n7T1++fP7w6u6xhfk24qwo+YpbwSVn4rqDZy9fOX90y6wyXw1+tLzFyMqvZBWcUdHUXJ0b6agh'
-			+ 'zI6ZgxlZuIRlVbW11eXFeFhxZHAmZlY2dlYWlPwPAD6nKPWk11d/AAAAAElFTkSuQmCC';
+  L.TileLayer.Mask = L.TileLayer.extend({
+    options: {
+      maskUrl: defaultMaskUrl,
+      maskSize: 512
+    },
+    getMaskSize: function() {
+      var s = this.options.maskSize;
+      return s instanceof L.Point ? s : new L.Point(s, s);
+    },
+    setCenter: function(containerPoint) {
+      if (arguments.length === 2) {
+        this.setCenter(L.point(arguments[0], arguments[1]));
+        return;
+      }
+      if (this._map) {
+        var p = this._map.containerPointToLayerPoint(containerPoint);
+        p = p.subtract(this.getMaskSize().divideBy(2));
+        this._image.setAttribute("x", p.x);
+        this._image.setAttribute("y", p.y);
+      }
+    },
+    _initContainer: function() {
+      if (this._container) return;
+      var rootGroup = this._map.getRenderer(this)._rootGroup;
+      var defs = rootGroup.appendChild(L.SVG.create("defs"));
+      var container = rootGroup.appendChild(L.SVG.create("g"));
+      var mask = defs.appendChild(L.SVG.create("mask"));
+      var image = mask.appendChild(L.SVG.create("image"));
+      var size = this.getMaskSize();
+      mask.setAttribute("id", "leaflet-tilelayer-mask-" + L.stamp(this));
+      image.setAttribute("width", size.x);
+      image.setAttribute("height", size.y);
+      image.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", this.options.maskUrl);
+      rootGroup.setAttribute("mask", "url(#" + mask.getAttribute("id") + ")");
+      this._container = container;
+      this._image = image;
+      this.setCenter(this._map.getSize().divideBy(2));
+    },
+    _updateLevels: function() {
 
-	var count = new Date().getTime();
-	var $e = function(name, attr) {
-		var id = "_leaflet_tilelayer_mask_" + (count++);
-		if (document.getElementById(id) != null)
-			return $e(name, attr);
-		var e = document.createElementNS("http://www.w3.org/2000/svg", name);
-		e.setAttribute("id", id);
-		if (attr)
-			for (key in attr)
-				if (key == "href")
-					e.setAttributeNS("http://www.w3.org/1999/xlink", key, attr[key]);
-				else
-					e.setAttribute(key, attr[key]);
-		return e;
-	};
+      var zoom = this._tileZoom;
+      if (zoom === undefined)
+        return undefined;
 
-	// Main
+      for (var z in this._levels) {
+        if (!this._levels[z].el.firstChild && z !== zoom) {
+          L.DomUtil.remove(this._levels[z].el);
+          this._removeTilesAtZoom(z);
+          delete this._levels[z];
+        }
+      }
 
-	L.TileLayer.Mask = L.TileLayer.extend({
-		options : {
-			maskWidth : 512,
-			maskHeight : 512,
-			maskUrl : maskUrlDefault
-		},
-		_tileEventHandler : {
-			"tileload" : function(e) {
-				if (this._e5) {
-					var p = L.DomUtil.getPosition(e.tile);
-					e.tile._svgimg = $e("image", {
-						x : p.x,
-						y : p.y,
-						width : this.options.tileSize,
-						height : this.options.tileSize,
-						href : e.url
-					});
-					this._e5.appendChild(e.tile._svgimg);
-				}
-			},
-			"tileunload" : function(e) {
-				var img = e.tile._svgimg;
-				if (img && img.parentNode)
-					img.parentNode.removeChild(img);
-			}
-		},
-		_mapEventHandler : {
-			"move" : function(e) {
-				if (this._e7 && this._map) {
-					var p = this._map.containerPointToLayerPoint([ 0, 0 ]);
-					this._e7.setAttribute("x", -p.x);
-					this._e7.setAttribute("y", -p.y);
-				}
-			},
-			"viewreset" : function(e) {
-				if (this._e5)
-					while (this._e5.firstChild)
-						this._e5.removeChild(this._e5.firstChild);
-			},
-			"zoomstart" : function(e) {
-				if (this._e1)
-					this._e1.style.opacity = 0.5;
-			},
-			"zoomend" : function(e) {
-				if (this._e1)
-					this._e1.style.opacity = 1.0;
-			}
-		},
-		onRemove : function(map) {
-			L.TileLayer.prototype.onRemove.call(this, map);
-			if (this._e1 && this._e1.parentNode)
-				this._e1.parentNode.removeChild(this._e1);
-			this.off(this._tileEventHandler, this);
-			map.off(this._mapEventHandler, this);
-		},
-		onAdd : function(map) {
-			L.TileLayer.prototype.onAdd.call(this, map);
-			this.getContainer().style.display = "none";
-			this.on(this._tileEventHandler, this);
-			map.on(this._mapEventHandler, this);
+      var level = this._levels[zoom];
+      if (!level) {
+        var map = this._map;
+        level = {
+          el: this._container.appendChild(L.SVG.create("g")),
+          origin: map.project(map.unproject(map.getPixelOrigin()), zoom).round(),
+          zoom: zoom
+        };
+        this._setZoomTransform(level, map.getCenter(), map.getZoom());
+        this._levels[zoom] = level;
+      }
+      this._level = level;
+      return level;
+    },
+    _addTile: function(coords, container) {
+      var tilePos = this._getTilePos(coords);
+      var tileSize = this.getTileSize();
+      var key = this._tileCoordsToKey(coords);
+      var url = this.getTileUrl(this._wrapCoords(coords));
 
-			this._e1 = $e("svg", {
-				width : "100%",
-				height : "100%",
-				style : "pointer-events:none;position:relative;"
-			});
-			this._e2 = $e("defs");
-			this._e3 = $e("mask", {
-				maskUnits : "userSpaceOnUse"
-			});
-			this._e4 = $e("image", {
-				width : this.options.maskWidth,
-				height : this.options.maskHeight,
-				href : this.options.maskUrl,
-				preserveAspectRatio : "none"
-			});
-			this._e5 = $e("g");
-			this._e6 = $e("g", {
-				mask : "url(#" + this._e3.getAttribute("id") + ")"
-			});
-			this._e7 = $e("use", {
-				href : "#" + this._e5.getAttribute("id")
-			});
-			this._e1.appendChild(this._e2);
-			this._e2.appendChild(this._e3);
-			this._e3.appendChild(this._e4);
-			this._e2.appendChild(this._e5);
-			this._e1.appendChild(this._e6);
-			this._e6.appendChild(this._e7);
+      var tile = container.appendChild(L.SVG.create("image"));
+      tile.setAttribute("width", tileSize.x);
+      tile.setAttribute("height", tileSize.y);
+      tile.setAttribute("x", tilePos.x);
+      tile.setAttribute("y", tilePos.y);
+      tile.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", url);
 
-			var cnt = map.getContainer();
-			cnt.appendChild(this._e1);
-			this.setCenter(cnt.clientWidth * 0.5, cnt.clientHeight * 0.5);
-		},
-		setCenter : function(x, y) {
-			if (this._e4) {
-				this._e4.setAttribute("x", x - this.options.maskWidth * 0.5);
-				this._e4.setAttribute("y", y - this.options.maskHeight * 0.5);
-			}
-			return this;
-		}
-	});
+      this._tiles[key] = {
+        el: tile,
+        coords: coords,
+        current: true
+      };
+    }
+  });
 
-	L.tileLayer.mask = function(url, options) {
-		return new L.TileLayer.Mask(url, options);
-	};
-})(window);
+  L.tileLayer.mask = function(url, options) {
+    return new L.TileLayer.Mask(url, options);
+  };
+
+})();
